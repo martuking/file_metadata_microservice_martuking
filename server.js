63e1451a61f445292b454c1485a538336d514af9
System <none>
2017-08-19 20:47:27 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,21 +4,16 @@ var cors = require('cors');
 var bodyparser = require('body-parser');
 var app = express();
 var upload = multer({ dest: 'uploads/' });
-app.use(express.static('public'));
 app.use(bodyparser.json());
 app.use(cors());
-app.get("/", function (request, response) {
+app.use(express.static(__dirname + '/views'));
+/*app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
-});
+});*/
 app.post('/upload',upload.single('file'),function(request,response,next){
-  
+return response.json(request.file)
 });
 
-
-
-
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
